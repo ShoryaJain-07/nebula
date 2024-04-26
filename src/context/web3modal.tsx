560ded8d796c47c3d532c47d9a8eb151ce41911a
Shorya Jain <shoryajain0708@gmail.com>
2024-04-26 21:16:48 +0530

    add-connect-wallet

---
+++ src/context/web3modal.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "6016c6b50f7ae06bc3b9d2cb7ea42b56";
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
+
+// 3. Create a metadata object
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: "...", // used for the Coinbase SDK
+  defaultChainId: 1, // used for the Coinbase SDK
+});
+
+// 5. Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig,
+  chains: [mainnet],
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+  allowUnsupportedChain: true,
+  includeWalletIds: [
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
+    "fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa",
+  ],
+});
+
+export function Web3Modal({ children}:{children : React.ReactNode} ) {
+  return children;
+}
